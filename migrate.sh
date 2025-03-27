1326f686a2f880dc2ff13c12d4201298aa6906dd
ahbarrozo <barrozo@tutanota.com>
2025-03-27 13:01:39 +0100

    Switch host to variable

--- migrate.sh
+++ migrate.sh
@@ -10,6 +10,7 @@ set -e  # Exit immediately if a command exits with a non-zero status
 
 DB_USER="thiago_brisolla"
 DB_NAME="thiago_brisolla"
+DB_HOST="localhost"
 
 # Prompt for password (input will be hidden)
 read -sp "Enter PostgreSQL password for $DB_USER : " PGPASSWORD
@@ -20,11 +21,11 @@ export PGPASSWORD
 
 # Check if schema_migrations table exists, create if it doesn't
 echo "Checking if schema_migrations table exists..."
-table_exists=$(psql -U $DB_USER -h localhost -d $DB_NAME -t -c "SELECT EXISTS (SELECT FROM information_schema.tables WHERE table_schema = 'public' AND table_name = 'schema_migrations')")
+table_exists=$(psql -U $DB_USER -h $DB_HOST -d $DB_NAME -t -c "SELECT EXISTS (SELECT FROM information_schema.tables WHERE table_schema = 'public' AND table_name = 'schema_migrations')")
 
 if [[ $table_exists == *"f"* ]]; then
   echo "Creating schema_migrations table..."
-  psql -U $DB_USER -h localhost -d $DB_NAME -c "CREATE TABLE schema_migrations (
+  psql -U $DB_USER -h $DB_HOST -d $DB_NAME -c "CREATE TABLE schema_migrations (
     version TEXT PRIMARY KEY,
     applied_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
   )"
@@ -37,10 +38,10 @@ fi
 echo "Checking for pending migrations..."
 for file in migrations/*.sql; do
   version=$(basename "$file" .sql) #strips file extension and directories from the file path
-  if ! psql -U $DB_USER -h localhost -d $DB_NAME -t -c "SELECT 1 FROM schema_migrations WHERE version = '$version'" | grep -q 1; then
+  if ! psql -U $DB_USER -h $DB_HOST -d $DB_NAME -t -c "SELECT 1 FROM schema_migrations WHERE version = '$version'" | grep -q 1; then
     echo "Applying migration: $file"
-    psql -U $DB_USER -h localhost -d $DB_NAME -f "$file"
-    psql -U $DB_USER -h localhost -d $DB_NAME -c "INSERT INTO schema_migrations (version) VALUES ('$version')"
+    psql -U $DB_USER -h $DB_HOST -d $DB_NAME -f "$file"
+    psql -U $DB_USER -h $DB_HOST -d $DB_NAME -c "INSERT INTO schema_migrations (version) VALUES ('$version')"
   fi
 done
 
